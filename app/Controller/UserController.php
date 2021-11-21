74370f5507c716e69e03bb62e2bc19e753192479
coki0291 <slherrera91@gmail.com>
2021-11-20 21:11:12 -0300

    Se cambió el número de rol en la función registerUser

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -55,7 +55,7 @@ class UserController {
         if (AuthHelper::checkLoggedOut()){
             $email = $_POST['email'];
             $contraseña = $_POST['password'];
-            $rol = 3;
+            $rol = 2;
             $hash = password_hash($contraseña, PASSWORD_BCRYPT);
             $this->model->addUser($email, $hash, $rol);
             $this->verifyLogin();
