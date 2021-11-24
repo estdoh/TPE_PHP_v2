63b64799cf2184af7751262976e0f90f0eb9ce7b
coki0291 <slherrera91@gmail.com>
2021-11-23 22:44:54 -0300

    Se agregó el control de imágenes en add y edit product

--- app/Controller/ProductController.php
+++ app/Controller/ProductController.php
@@ -14,28 +14,22 @@ class ProductsController {
     private $authHelper;
 
     public function __construct() {
-        $authHelper = new AuthHelper();        
         $this->model = new ProductsModel();
         $this->modelCategories = new CategoryModel();
         $this->view = new ProductsView();
         $this->modelComments = new CommentsModel();
     }    
 
-    function  showProducts2(){
-        $productos = $this->model->getProducts();
-        return $this->view->response($products, 200);
-    }
-
-    function showProducts() {        
+    function showProducts() {
         AuthHelper::start(); 
         $products = $this->model->getProducts();
-        $categories = $this->modelCategories->getCategories();        
+        $categories = $this->modelCategories->getCategories();
         $this->view->viewProducts($products, $categories);
     }
 
-    function delProducts($params = null) {
+    function delProducts($params = null) {     
         if (AuthHelper::checkLoggedIn()){
-            $this->model->deleteProducts($params);            
+            $this->model->deleteProducts($params);
             header("Location: ".BASE_URL."showProducts");
         }
     }
@@ -62,57 +56,51 @@ class ProductsController {
                 $this->showError("El nombre o la categoría no pueden ser vacios");
             }
             else{
-                $imagen_name = $_FILES['input_image']['tmp_name'];
-                $imagen_type = $_FILES['input_image']['type'];
-                $pathImg = null;
-                $pathImg = $this->uploadImage($imagen_name);
-
-                $this->model->addProduct($category, $name, $description, $pathImg, $price_a, $price_b);
+                if($_FILES['input_image']['type'] == "image/jpg" || $_FILES['input_image']['type'] == "image/jpeg" 
+                    || $_FILES['input_image']['type'] == "image/png" ) {
+                    $this->model->addProduct($category, $name, $description, $price_a, $price_b,$_FILES['input_image']);
+                }
+                else {
+                    $this->model->addProduct($category, $name, $description, $price_a, $price_b);
+                }        
                 header("Location: ".BASE_URL."showProducts");
             }
-        }
-            // if( $imagen_type == "image/jpg" || $imagen_type == "image/jpeg" || $imagen_type == "image/png" || $imagen_type == "image/gif" ){
-            //     $this->model->addProduct($name, $description, $imagen_name, $category, $price_a, $price_b);
-            //     header("Location: ".BASE_URL."showProducts");
-            // } 
-            // else {
-            //     $this->model->addProduct($name, $description, $category, $price_a, $price_b);
-            //     header("Location: ".BASE_URL."showProducts");
-            // }            
+        }   
         else {
             header("Location: ".BASE_URL."login");
         }
     }
 
     function editProduct($id){
-        $name = $_POST['input_name'];
-        $category = $_POST['input_category'];
-        $description = $_POST['input_description'];
-        if (isset($_POST['input_category'])){
-            $category = $_POST['input_category'];
-        }
+        if (AuthHelper::checkLoggedIn()){
+            $name = $_POST['input_name'];
+            $category = "";
+            $description = $_POST['input_description'];
+            if (isset($_POST['input_category'])){
+                $category = $_POST['input_category'];
+            }
 
-        $price_a = $_POST['input_price_a'];
-        $price_b = $_POST['input_price_b'];
+            $price_a = $_POST['input_price_a'];
+            $price_b = $_POST['input_price_b'];
 
-        if($name == "" || $category == ""){
-            $this->showError("El nombre o la categoría no pueden ser vacios");
+            if($name == "" || $category == ""){
+                $this->showError("El nombre o la categoría no pueden ser vacios");
+            }
+            else{
+                if($_FILES['input_image']['type'] == "image/jpg" || $_FILES['input_image']['type'] == "image/jpeg" 
+                || $_FILES['input_image']['type'] == "image/png" ) {
+                    $this->model->updateProductById($category, $name, $description, $price_a, $price_b, $id,$_FILES['input_image']);
+                }
+                else {
+                    $this->model->updateProductById($category, $name, $description, $price_a, $price_b,$id);
+                }        
+                header("Location: ".BASE_URL."showProducts");
+        
+            }
         }
-        else{
-            $imagen_name = $_FILES['input_image']['tmp_name'];//$this->addImage();
-            $imagen_type = $_FILES['input_image']['type'];
-            $pathImg = null;
-            $pathImg = $this->uploadImage($imagen_name);
-            // $this->model->updateProductById($category,$name,$description,$price_a,$price_b,$id);
-            $this->model->updateProductById($category, $name, $description, $pathImg, $price_a, $price_b, $id);
-            header("Location: ".BASE_URL."showProducts");
+        else {
+            header("Location: ".BASE_URL."login");
         }
-    }
-
-    private function uploadImage($imagen_name){
-        $uploads_dir = 'images/' . uniqid() . '.png';
-        move_uploaded_file($imagen_name, $uploads_dir);
-        return $uploads_dir;
     }
 
     function presupuestar() {  
@@ -124,7 +112,6 @@ class ProductsController {
     function viewProduct($id = null){              
         $product = $this->model->getProductById($id);
         $categories = $this->modelCategories->getCategories($id);
-        // $comments = $this->modelComments->getComments($id);
         $this->view->viewPageProduct($product, $categories);
     }
 
